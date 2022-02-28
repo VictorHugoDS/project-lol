447308de750e5337fd3382ec2ef1e3390b414341
unknown <victorhugod.s@hotmail.com>
2022-02-28 19:43:09 -0400

    Cria a base do projeto, começa criação dos primeiros componentes

---
+++ components/moduraCampeao/selecao.js
@@ -0,0 +1,11 @@
+import react from "react"
+export default function Selecao() {
+  return (
+    <>
+        <div className="selecao">
+          componente bacanudo
+        </div>
+        <input/>
+    </>
+  )
+}
